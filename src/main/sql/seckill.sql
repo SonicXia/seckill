a533fb25d39227f75e040e0477d18719c169f812
Sonic <sonicdxf@gmail.com>
2017-10-14 16:38:28 +0800

    添加sql存储过程

---
+++ src/main/sql/seckill.sql
@@ -0,0 +1,20 @@
+-- 秒杀执行存储过程
+DELIMITER $$ -- console ; 转换成 $$
+-- 定义存储过程
+-- 参数：in 输入参数；out 输出参数
+-- row_count():返回上一条修改类型sql(delete,insert,update)的影响行数
+-- row_count(): 0:未修改数据；>0:表示修改的行数；<0:sql错误/未执行修改sql
+CREATE PROCEDURE `seckill`.`execute_seckill`
+  (IN v_seckill_id bigint, IN v_phone bigint, IN v_kill_time timestamp , OUT r_result int)
+  BEGIN
+    DECLARE insert_count INT DEFAULT 0;
+    START TRANSACTION ;
+    INSERT ignore INTO success_killed
+      (seckill_id, user_phone, ceate_time)
+    VALUES (v_seckill_id, v_phone, v_kill_time);
+    SELECT ROW_COUNT() INTO insert_count;
+    IF (insert_count < 0) THEN
+      ROLLBACK;
+      SET r_result = -1;
+
+
